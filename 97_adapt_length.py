9e0677c3878881a64ac44e3159fdb73633babaff
Gabriel Felipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-03-09 20:56:34 -0300

    Create 97_adapt_length.py

---
+++ 97_adapt_length.py
@@ -0,0 +1,9 @@
+def escreva(text):
+    
+    print('-' * len(text))
+    print(text)
+    print('-' * len(text))
+    
+escreva("Gustavo Guanabara")   
+escreva("Curso de python no Youtube") 
+escreva("CV") 
