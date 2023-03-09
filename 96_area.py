e24ccd98bd35f83dc95c6bcbe413e8c5820540cc
Gabriel Felipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-03-09 20:54:12 -0300

    Update 96_area.py

--- 96_area.py
+++ 96_area.py
@@ -1,3 +1,9 @@
 def area(width, height):
     
     print(f'A área do terreno é {width * height}')
+    
+    
+width = float(input('Type the width: '))
+height = float(input('Type the height: '))
+
+area(width, height)
