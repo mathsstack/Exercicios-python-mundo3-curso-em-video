0243a900098fe1984ffe337e78a778cf7a56ebf1
Gabriel Felipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-03-13 12:11:21 -0300

    Create 108_module.py

---
+++ 108_module.py
@@ -0,0 +1,19 @@
+def aumentar(value, qtde):
+    
+    return value + qtde
+    
+def diminuir(value, qtde):
+    
+    return value - qtde
+    
+def dobrar(value):
+    
+    return value * 2
+    
+def metade(value):
+    
+    return value / 2
+    
+def moeda(value):
+    
+    return 'R$ ' + str(value) + ',00'
